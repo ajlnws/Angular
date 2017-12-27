f3f4f31186bc8cd34cbfde08682b80532613a74a
Adrià Julià Newshore <adria.julia@newshore.es>
2017-12-27 12:34:05 +0100

    json

--- Portafolio/src/app/app.module.ts
+++ Portafolio/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 // Rutas
 import { app_routing } from "./app.routes";
+
+// Servicios
+import { InformacionService } from "./services/informacion.service";
 
 
 // Componentes
@@ -26,9 +29,10 @@ import { ItemComponent } from './components/item/item.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
   ],
-  providers: [],
+  providers: [InformacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
